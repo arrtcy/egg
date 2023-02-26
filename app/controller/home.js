ae37a3bc4d814231ba9626cb20699a3e87c88b0e
tianchongyang <wb.tianchongyang@mesg.corp.netease.com>
2023-02-26 22:52:20 +0800

    a

--- app/controller/home.js
+++ app/controller/home.js
@@ -7,6 +7,11 @@ class HomeController extends Controller {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
+
+  async show() {
+    const { ctx } = this;
+    ctx.body = 'show 返回';
+  }
 }
 
 module.exports = HomeController;
